c584a011a7295e70e46810f02926b752c3c6eb3b
TecStylos <tecstylos@gmail.com>
2022-04-03 20:33:24 +0200

    Moved func 'execCmd' into separate files

---
+++ src/ExecCmd.cpp
@@ -0,0 +1,53 @@
+#include "ExecCmd.h"
+
+#include "Errors/QinpError.h"
+
+#if defined QINP_PLATFORM_UNIX
+
+#include <sys/wait.h>
+
+int execCmd(const std::string& command)
+{
+	pid_t p = fork();
+
+	if (p == -1)
+		THROW_QINP_ERROR("Fork failed!");
+	
+	if (p == 0)
+	{
+		execl("/bin/sh", "sh", "-c", command.c_str(), nullptr);
+		THROW_QINP_ERROR("Exec failed!");
+	}
+
+	int status;
+	if (waitpid(p, &status, 0) == -1)
+		THROW_QINP_ERROR("Waitpid failed!");
+
+	if (WIFEXITED(status))
+		return WEXITSTATUS(status);
+	else
+		return -1;
+}
+
+#elif defined QINP_PLATFORM_WINDOWS
+
+#include <windows.h>
+
+int execCmd(const std::string& command)
+{
+	STARTUPINFO si;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&pi, sizeof(pi));
+
+	BOOL b = CreateProcessA("C:\\Windows\\System32\\cmd.exe", (LPSTR)command.c_str(), NULL, NULL, TRUE, NULL, NULL, NULL, &si, &pi);
+	WaitForSingleObject(pi.hProcess, INFINITE);
+
+	DWORD dwExitCode = 0;
+	GetExitCodeProcess(pi.hProcess, &dwExitCode);
+
+	return dwExitCode;
+}
+
+#endif
---
+++ src/ExecCmd.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <string>
+
+int execCmd(const std::string& command);
