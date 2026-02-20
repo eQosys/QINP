ba18e3c62601246eda0dcff79572c1edb0731a40
TecStylos <v.danninger@tum.de>
2026-02-20 16:59:49 +0100

    Fixed compile issue

--- src/Symbols.cpp
+++ src/Symbols.cpp
@@ -2,6 +2,7 @@
 
 #include <cassert>
 #include <algorithm>
+#include <cstdint>
 
 #include "Errors/ProgGenError.h"
 
--- src/Token.h
+++ src/Token.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <memory>
 #include <map>
+#include <cstdint>
 
 struct Token
 {
